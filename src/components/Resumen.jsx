6c9b3be43691acca7de4e58b8faa08a9179a4e47
Julián Moreno <julianmoreno263@gmail.com>
2024-03-11 17:29:09 -0500

    Terminando el front end con react,terminando de crear la parte del resumen del pedido

--- src/components/Resumen.jsx
+++ src/components/Resumen.jsx
@@ -1,9 +1,13 @@
+import { formatearDinero } from '../helpers'
 import useQuiosco from '../hooks/useQuiosco'
 import ResumenProducto from './ResumenProducto'
 
 export default function Resumen() {
 
-    const { pedido } = useQuiosco()
+    const { pedido, total } = useQuiosco()
+
+    //si el pedido esta en cero aqui me retorna true
+    const comprobarPedido = () => pedido.length === 0
 
     return (
         <aside className="w-72 h-screen overflow-y-scroll p-5">
@@ -25,15 +29,16 @@ export default function Resumen() {
                 )}
             </div>
 
-            <p className='text-xl mt-10'>Total: {''}</p>
+            <p className='text-xl mt-10'>Total: {''} {formatearDinero(total)}</p>
 
             <form action="" className="w-full">
                 <div className="mt-5">
                     <input
-                        className='bg-indigo-600 hover:bg-indigo-800 px-5 py-2 rounded uppercase 
-                    font-bold text-white textcenter w-full cursor-pointer'
+                        className={`${comprobarPedido() ? 'bg-indigo-100' : 'bg-indigo-600 hover:bg-indigo-800'}
+                        px-5 py-2 rounded uppercase font-bold text-white textcenter w-full cursor-pointer`}
                         type="submit"
                         value="Confirmar Pedido"
+                        disabled={comprobarPedido()}
                     />
                 </div>
             </form>
--- src/context/QuioscoProvider.jsx
+++ src/context/QuioscoProvider.jsx
@@ -1,5 +1,5 @@
 
-import { createContext, useState } from "react"
+import { createContext, useState, useEffect } from "react"
 import { toast } from "react-toastify"
 import { categorias as categoriasDB } from "../data/categorias"
 
@@ -14,6 +14,14 @@ const QuioscoProvider = ({ children }) => {
     const [modal, setModal] = useState(false)
     const [producto, setProducto] = useState({})
     const [pedido, setPedido] = useState([])
+    const [total, setTotal] = useState(0)
+
+
+    //useEffect para ir calculando el total del pedido,utilizamos el array method reduce()
+    useEffect(() => {
+        const nuevoTotal = pedido.reduce((total, producto) => (producto.precio * producto.cantidad) + total, 0)
+        setTotal(nuevoTotal)
+    }, [pedido])
 
 
 
@@ -34,7 +42,7 @@ const QuioscoProvider = ({ children }) => {
         setProducto(producto)
     }
 
-    const handleAgregarPedido = ({ categoria_id, imagen, ...producto }) => {
+    const handleAgregarPedido = ({ categoria_id, ...producto }) => {
 
         //setPedido es un array, si queremos agregar productos a este array no podemos utilizar el metodo push porque este metodo nos genera un nuevo array y eso modificaria el state original,eso no se puede hacer en react, entonces lo que hacemos es tomar una copia del pedido(...pedido) y agregarle un producto.
 
@@ -50,6 +58,23 @@ const QuioscoProvider = ({ children }) => {
         }
     }
 
+    const handleEditarCantidad = (id) => {
+
+        const productoActualizar = pedido.filter(producto => producto.id === id)[0]
+        setProducto(productoActualizar)
+        setModal(!modal)
+    }
+
+
+    const handleEliminarProductoPedido = (id) => {
+
+        const pedidoActualizado = pedido.filter(producto => producto.id !== id)
+        setPedido(pedidoActualizado)
+        toast.success('Eliminado del Pedido')
+
+    }
+
+
 
     return (
 
@@ -64,7 +89,10 @@ const QuioscoProvider = ({ children }) => {
                 producto,
                 handleSetProducto,
                 pedido,
-                handleAgregarPedido
+                handleAgregarPedido,
+                handleEditarCantidad,
+                handleEliminarProductoPedido,
+                total
             }}
 
         >{children}</QuioscoContext.Provider>
