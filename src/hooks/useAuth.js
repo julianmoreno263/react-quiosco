0aec0979fc40e924afad77625356967442feb336
Julián Moreno <julianmoreno263@gmail.com>
2024-04-05 17:20:49 -0500

    creando un hook para las funcionalidades de login,registro y logout

---
+++ src/hooks/useAuth.js
@@ -0,0 +1,67 @@
+import { useEffect } from 'react'
+import useSWR from 'swr'
+import { useNavigate } from 'react-router-dom'
+import clienteAxios from "../config/axios"
+
+
+export const useAuth = ({ middleware, url }) => {
+
+    const token = localStorage.getItem('AUTH_TOKEN')
+    const navigate = useNavigate()
+
+    const { data: user, error, mutate } = useSWR('/api/user', () =>
+
+        clienteAxios('/api/user', {
+
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        })
+            .then(res => res.data)
+            .catch(error => {
+                throw Error(error?.response?.data?.errors)
+            })
+    )
+
+    const login = async (datos, setErrores) => {
+
+        try {
+            const { data } = await clienteAxios.post('/api/login', datos)
+            localStorage.setItem('AUTH_TOKEN', data.token)
+            setErrores([])
+            await mutate()
+        } catch (error) {
+            setErrores(Object.values(error.response.data.errors))
+        }
+    }
+
+    const registro = () => {
+
+    }
+
+    const logout = () => {
+
+    }
+
+    console.log(user)
+    console.log(error)
+
+    console.log(middleware)
+    console.log(url)
+
+    useEffect(() => {
+
+        if (middleware === 'guest' && url && user) {
+            navigate(url)
+        }
+
+    }, [user, error])
+
+    return {
+
+        login,
+        registro,
+        logout
+    }
+
+}
